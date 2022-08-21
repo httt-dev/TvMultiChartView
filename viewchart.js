d66b76fa5efcdbf3c32f9a91acfa7bd235131b31
Hoa Nguyen <httt.dev@gmail.com>
2022-08-21 08:47:35 +0700

    unknow commit change

--- viewchart.js
+++ viewchart.js
@@ -1,4 +1,30 @@
 
+const Symbols = {
+    symbol1 :"",
+    symbol2 :"",
+    symbol3 :"",
+    symbol4 :"",
+    symbol5 :"",
+    symbol6 :"",
+    symbol7 :"",
+    symbol8 :"",
+    symbol9 :"",
+    symbol10 :"",
+    symbol11 :"",
+    symbol12 :"",
+    symbol13 :"",
+    symbol14 :"",
+    symbol15 :"",
+    symbol16 :"",
+    numberOfColsOnRow : "1",
+    chkHideIndiRsi : false,
+    chkHideLeftToolbar:false,
+    chkHideIndiBB : false
+};
+
+var PairSymbols = Symbols;
+var rows_by_symbol_input = 0;
+
 function generateRandomInteger(max) {
     return Math.floor(Math.random() * max) + 1;
 }
@@ -9,6 +35,7 @@ const TvChart = {
     symbolsCount:0,
     chkHideIndiRsi:false,
     chkHideLeftToolbar:false,
+    chkHideIndiBB:false,
     //row_height : "30vh",
     init : function(){
         var div_main_container = document.getElementById('container');
@@ -45,11 +72,15 @@ const TvChart = {
                                                     <span class="blue-text">XRPUSDT Chart</span>
                                                     </a> by TradingView`;
                     
-                    var indiList;
+                    var indiStochasticRSI="";
                     if (!this.chkHideIndiRsi){
-                        indiList = "StochasticRSI@tv-basicstudies";
+                        indiStochasticRSI = "StochasticRSI@tv-basicstudies";
                     }else{
-                        indiList = "";
+                        indiStochasticRSI = "";
+                    }
+                    var indiBB="";
+                    if (!this.chkHideIndiBB){
+                        indiBB = "BB@tv-basicstudies";
                     }
                     var mainHtmlTv = `<script type="text/javascript">
                                         new TradingView.widget(
@@ -67,7 +98,8 @@ const TvChart = {
                                                 "hide_side_toolbar":`+this.chkHideLeftToolbar+`,
                                                 "allow_symbol_change": true,
                                                 "studies": [
-                                                    "`+indiList+`"
+                                                    "`+indiStochasticRSI+`",
+                                                    "`+ indiBB + `",
                                                   ],
                                                 "container_id": "`+tv_widget_id+`"
                                             }
@@ -142,28 +174,6 @@ const TvChart = {
 
 };
 
-const Symbols = {
-    symbol1 :"",
-    symbol2 :"",
-    symbol3 :"",
-    symbol4 :"",
-    symbol5 :"",
-    symbol6 :"",
-    symbol7 :"",
-    symbol8 :"",
-    symbol9 :"",
-    symbol10 :"",
-    symbol11 :"",
-    symbol12 :"",
-    symbol13 :"",
-    symbol14 :"",
-    symbol15 :"",
-    symbol16 :"",
-    numberOfColsOnRow : "1",
-    chkHideIndiRsi : false,
-    chkHideLeftToolbar:false
-};
-
 
 const SymbolListStorage ={
     getSymbolsInput: function(){
@@ -179,34 +189,82 @@ const SymbolListStorage ={
         symbolStr["numberOfColsOnRow"] = document.querySelector('input[name="radNumberOfColsOnRow"]:checked').id ;
         symbolStr["chkHideIndiRsi"] = document.getElementById("chkHideIndiRsi").checked;
         symbolStr["chkHideLeftToolbar"] = document.getElementById("chkHideLeftToolbar").checked;
+        symbolStr["chkHideIndiBB"] = document.getElementById("chkHideIndiBB").checked;
         return symbolStr;
     },
-    saveSymbolsInput:function(){
+    saveSymbolsInput:function(txtName){
         const symbolsInput = this.getSymbolsInput();
-        console.log(symbolsInput);
-        window.localStorage.removeItem('symbols');
-        window.localStorage.setItem('symbols',JSON.stringify(symbolsInput));
+        //console.log(symbolsInput);
+        if(txtName=="" || txtName==undefined)
+            txtName="symbols";
+
+        window.localStorage.removeItem(txtName);
+        window.localStorage.setItem(txtName,JSON.stringify(symbolsInput));
 
     },
-    readSymbolsStorage : function(){
-        const readSymbols = JSON.parse(window.localStorage.getItem('symbols'));
+    readSymbolsStorage : function(txtName){
+        if(txtName=="" || txtName==undefined)
+            txtName="symbols";
+
+        const readSymbols = JSON.parse(window.localStorage.getItem(txtName));
         return  readSymbols;
     },
-    
+    displayOnScreen : function (txtName){
+        PairSymbols = SymbolListStorage.readSymbolsStorage(txtName);
+        if(PairSymbols){
+            countSymbol =0;
+            //hien thi len man hinh
+            for(var pro in PairSymbols){
+                if(Object.prototype.hasOwnProperty.call(PairSymbols, pro)){
+                    if(pro=="numberOfColsOnRow"){
+                        document.getElementById(PairSymbols[pro]).checked = true;
+                    }else if(pro.substr(0,3)== "sym"){
+                        let symbolId = document.getElementById(pro);
+                        if(pro){
+                            symbolId.value = PairSymbols[pro];
+                            if(PairSymbols[pro]!=""){
+                                countSymbol++;
+                            }
+                        }
+                    }else if(pro== "chkHideIndiRsi"){
+                        let chkHideRsi = document.getElementById("chkHideIndiRsi");
+                        if(chkHideRsi){
+                            chkHideRsi.checked = PairSymbols[pro];
+                        }
+                        
+                    }else if(pro== "chkHideLeftToolbar"){
+                        let chkHideLeftTb = document.getElementById("chkHideLeftToolbar");
+                        if(chkHideLeftTb){
+                            chkHideLeftTb.checked = PairSymbols[pro];
+                        }
+                    }else if(pro== "chkHideIndiBB"){
+                        let chkHideIndiBb = document.getElementById("chkHideIndiBB");
+                        if(chkHideIndiBb){
+                            chkHideIndiBb.checked = PairSymbols[pro];
+                        }
+                    }
+
+                    
+                }
+            } 
+
+        }
+    }
 };
+
+
 
 function SaveLocalStorage(){
     SymbolListStorage.saveSymbolsInput();
     document.getElementById('btnInput').click();
 }
 
-var PairSymbols = Symbols;
-var rows_by_symbol_input = 0;
 
 function ViewChart(){
     var countSymbol =0;
 
-    TvChart.symbols = SymbolListStorage.readSymbolsStorage();
+    //TvChart.symbols = SymbolListStorage.readSymbolsStorage();
+    TvChart.symbols = SymbolListStorage.getSymbolsInput();
     //tinh toan ol va row dua vao so lieu da input 
     TvChart.cols = document.querySelector('input[name="radNumberOfColsOnRow"]:checked').id.substr(6,1);
     Object.getOwnPropertyNames(TvChart.symbols).forEach(key => {
@@ -219,46 +277,148 @@ function ViewChart(){
     TvChart.rows = Math.ceil(countSymbol/TvChart.cols);
     TvChart.chkHideIndiRsi = TvChart.symbols["chkHideIndiRsi"];
     TvChart.chkHideLeftToolbar = TvChart.symbols["chkHideLeftToolbar"];
+    TvChart.chkHideIndiBB = TvChart.symbols["chkHideIndiBB"];
+
     TvChart.init();
     TvChart.create_multi_chart();
 }
 
+function closeOneModal(modalId) {
+
+    // get modal
+    const modal = document.getElementById(modalId);
+
+    // change state like in hidden modal
+    modal.classList.remove('show');
+    modal.setAttribute('aria-hidden', 'true');
+    modal.setAttribute('style', 'display: none');
+
+     // get modal backdrop
+     const modalBackdrops = document.getElementsByClassName('modal-backdrop');
+
+     // remove opened modal backdrop
+      document.body.removeChild(modalBackdrops[0]);
+}
+
+function allStorage() {
+
+    var archive = [],
+        keys = Object.keys(localStorage),
+        i = 0, key;
+
+    for (; key = keys[i]; i++) {
+        archive.push( key + '=' + localStorage.getItem(key));
+    }
+
+    //return archive;
+    console.log(archive);
+    return archive;
+}
+
+function loadFavourite(){
+    alert("loadFavourite");
+}
+
 window.onload = function(){
 
-    PairSymbols = SymbolListStorage.readSymbolsStorage();
-    if(PairSymbols){
-        countSymbol =0;
-        //hien thi len man hinh
-        for(var pro in PairSymbols){
-            if(Object.prototype.hasOwnProperty.call(PairSymbols, pro)){
-                if(pro=="numberOfColsOnRow"){
-                    document.getElementById(PairSymbols[pro]).checked = true;
-                }else if(pro.substr(0,3)== "sym"){
-                    let symbolId = document.getElementById(pro);
-                    if(pro){
-                        symbolId.value = PairSymbols[pro];
-                        if(PairSymbols[pro]!=""){
-                            countSymbol++;
-                        }
-                    }
-                }else if(pro== "chkHideIndiRsi"){
-                    let chkHideRsi = document.getElementById("chkHideIndiRsi");
-                    if(chkHideRsi){
-                        chkHideRsi.checked = PairSymbols[pro];
-                    }
+    // PairSymbols = SymbolListStorage.readSymbolsStorage();
+    // if(PairSymbols){
+    //     countSymbol =0;
+    //     //hien thi len man hinh
+    //     for(var pro in PairSymbols){
+    //         if(Object.prototype.hasOwnProperty.call(PairSymbols, pro)){
+    //             if(pro=="numberOfColsOnRow"){
+    //                 document.getElementById(PairSymbols[pro]).checked = true;
+    //             }else if(pro.substr(0,3)== "sym"){
+    //                 let symbolId = document.getElementById(pro);
+    //                 if(pro){
+    //                     symbolId.value = PairSymbols[pro];
+    //                     if(PairSymbols[pro]!=""){
+    //                         countSymbol++;
+    //                     }
+    //                 }
+    //             }else if(pro== "chkHideIndiRsi"){
+    //                 let chkHideRsi = document.getElementById("chkHideIndiRsi");
+    //                 if(chkHideRsi){
+    //                     chkHideRsi.checked = PairSymbols[pro];
+    //                 }
                     
-                }else if(pro== "chkHideLeftToolbar"){
-                    let chkHideLeftTb = document.getElementById("chkHideLeftToolbar");
-                    if(chkHideLeftTb){
-                        chkHideLeftTb.checked = PairSymbols[pro];
-                    }
-                }
+    //             }else if(pro== "chkHideLeftToolbar"){
+    //                 let chkHideLeftTb = document.getElementById("chkHideLeftToolbar");
+    //                 if(chkHideLeftTb){
+    //                     chkHideLeftTb.checked = PairSymbols[pro];
+    //                 }
+    //             }else if(pro== "chkHideIndiBB"){
+    //                 let chkHideIndiBb = document.getElementById("chkHideIndiBB");
+    //                 if(chkHideIndiBb){
+    //                     chkHideIndiBb.checked = PairSymbols[pro];
+    //                 }
+    //             }
 
                 
-            }
-        } 
+    //         }
+    //     } 
 
+    // }
+
+
+    var btnSaveList = document.getElementById('btnSaveList');
+    var saveModal = document.getElementById('saveModal');
+
+    if(btnSaveList){
+
+        btnSaveList.addEventListener('click', function(event){
+            var txtName = document.getElementById('txtName');
+
+            SymbolListStorage.saveSymbolsInput(txtName.value);
+            bootstrap.Modal.getInstance(saveModal).hide();
+            document.getElementById('btnInput').click();
+        });
     }
+
+    //apply selected list 
+    var btnLoadFavourite = document.getElementById('btnLoadList');
+    var loadModalFavourite = document.getElementById('loadModal');
+    
+    if(btnLoadFavourite){
+        btnLoadFavourite.addEventListener('click', function(event){
+            //load 
+            //get selected option 
+            var selectOption = document.getElementById("listFavourite");
+            //var selectedItem = selectOption.options[selectOption.selectedIndex].text;
+
+            SymbolListStorage.displayOnScreen(selectOption.value);
+            bootstrap.Modal.getInstance(loadModalFavourite).hide();
+        }, { passive: true });
+    }
+   
+    //load favourite list when show modal
+    
+    loadModalFavourite.addEventListener('show.bs.modal', function(event){
+
+        // Button that triggered the modal
+        var buttonTarget = event.relatedTarget
+        const sb = document.querySelector('#listFavourite');
+        //load data from localstorage 
+        // iterate localStorage
+        sb.innerHTML="";
+        for (var i = 0; i < localStorage.length; i++) {
+
+            // set iteration key name
+            var key = localStorage.key(i);
+            const option = new Option(key, key);
+            sb.add(option , undefined);
+            // use key name to retrieve the corresponding value
+            //var value = localStorage.getItem(key);
+        
+            // console.log the iteration key and value
+            //console.log('Key: ' + key + ', Value: ' + value);  
+        
+        }
+        //event.stopPropagation();
+    });
+
+
     //TvChart.create_multi_chart();
 
     // //create div elemt 
